d52271b00cffd3410e33414531080ea0e6b1d050
Daniel Moldovan <d.moldovan@dsg.tuwien.ac.at>
2013-10-23 21:40:40 +0200

    Fixed bug which returned elasticity pathway instead of space

--- MELA-Core/MELA-AnalysisService/src/main/java/at/ac/tuwien/dsg/mela/analysisservice/apis/webAPI/MELA_WS.java
+++ MELA-Core/MELA-AnalysisService/src/main/java/at/ac/tuwien/dsg/mela/analysisservice/apis/webAPI/MELA_WS.java
@@ -100,7 +100,7 @@ public class MELA_WS {
     @Consumes("application/xml")
     @Produces("application/json")
     public String getLatestElasticitySpaceInJSON(MonitoredElement element) {
-        return systemControl.getElasticityPathway(element);
+        return systemControl.getElasticitySpace(element);
     }
 
     /**

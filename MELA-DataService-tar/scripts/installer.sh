86c2d0c89a3c080afd8bfa3a734566772e18308e
Daniel Moldovan <d.moldovan@dsg.tuwien.ac.at>
2014-01-08 15:58:38 +0100

    - Splited MELA code in Standalone and Celar-related, to inchrease its usability. Now it is easier to deploy MELA both outside and inside Celar, with minimum effort. - Standalone MELA moved in https://github.com/tuwiendsg/MELA, and Celar related MELA customization is left in https://github.com/CELAR/multilevel-metrics-evaluation. All MELA source files continue to bare the Celar acknowledgment, regardless of their repository. - Embedded Tomcat added to MELA-AnalysisService for easier deployment, w:wand scripts for generating MELA tarballs have been modified, to generate a ready to use MELA distribution. - MELA-AnalysisService does not longer control the DataCollection mechanism. This was moved to the MELA-DataService, reducing the AnalysisService's footprint.

---
+++ MELA-DataService-tar/scripts/installer.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+MELA_DATA_DIR=/opt/mela-data-service
+
+mkdir -p $MELA_DATA_DIR
+cp ./*jar $MELA_DATA_DIR
+cp -R ./dataServiceConfig $MELA_DATA_DIR
+
+cp ./mela-data-service /etc/init.d/
+chmod +x /etc/init.d/mela-data-service
+update-rc.d -f mela-data-service defaults
